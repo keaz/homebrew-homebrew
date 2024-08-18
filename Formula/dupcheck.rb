ba1fd5a3c64b5c2edad5ea7f3bfc68e16b0d35e3
Kasun <kasun.ranasinghe@icloud.com>
2024-08-18 22:46:24 +0530

    Added uplicate checker

---
+++ Formula/dupcheck.rb
@@ -0,0 +1,15 @@
+class Rfcp < Formula
+  desc "A duplicate file discovery tool written in Rust."
+  homepage "https://github.com/keaz/rust-duplicate-file-detector"
+  url "https://github.com/keaz/rust-duplicate-file-detector/releases/download/v2.0.2/duplicate-checker"
+  sha256 "fadbd42cd905a952ef235819e57598e80f48b92787f3bcb8c9aad78cab0aa571"
+  license "MIT/Apache-2.0"
+
+  def install
+    bin.install "duplicate-checker"
+  end
+
+  test do
+    system "#{bin}/duplicate-checker" "--help"
+  end
+end
