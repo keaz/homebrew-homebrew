593528005113d1c75acb31d5a364601eb28160fb
Kasun <kasun.ranasinghe@icloud.com>
2024-10-14 20:31:21 +0530

    Added sha of the universal binary

--- Formula/kcli.rb
+++ Formula/kcli.rb
@@ -2,8 +2,9 @@ class Kcli < Formula
   desc "A CLI tool to monitor Kafka topics and produce/consume messages"
   homepage "https://github.com/keaz/kcli"
   url "https://github.com/keaz/kcli/releases/download/v0.1.0-alpha/kcli"
-  sha256 "a248bd8f219afc4af6610d255c7aa3fbe0512dbf586e7cd86005f97a44fc56cc"
+  sha256 "37f729fd5bf899c01194cce8eb833d58cea8264f10ae6ca1f4ee741bf912d61a"
   license "GPL-3.0"
+  version "0.1.0-alpha"
 
   def install
     bin.install "kcli"
