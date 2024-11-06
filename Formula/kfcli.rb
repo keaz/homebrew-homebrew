554ce9e0e4b94bd996e00209cdc8f2eca9f6b668
Kasun Ranasinghe <Kasun.ranasinghe@icloud.com>
2024-11-06 22:04:01 +0530

    Update kfcli.rb

--- Formula/kfcli.rb
+++ Formula/kfcli.rb
@@ -3,6 +3,7 @@ class Kfcli < Formula
   homepage "https://github.com/keaz/kcli"
   url "https://github.com/keaz/kcli/releases/download/v0.0.1-alpha/kfcli-macos"
   version "v0.0.1-alpha"
+  sha "d271f279e9dc0ee0ed5a66f38be16acdbe67f7f6e17a1b86fc8e7f9fb4c84e97"
   license "GPL-3.0"
 
   def install
